620bec13da7be0f6b36887295049e96245d0e0eb
Kaan İhsan Eşkut <eskutkaan@gmail.com>
2018-04-09 08:57:42 +0300

    Add files via upload

--- investigate_patient.py
+++ investigate_patient.py
@@ -1,13 +1,15 @@
 file_object  = open("genestorage.txt", "r")
 temp_fileobj = file_object.readlines()
+for i in temp_fileobj:
+	if i == '\n':
+		temp_fileobj.remove(i)
 indexofptt = "Pt.ID:" + input("investigating patient:") + "\n"
-aavv= [i + 3 for i,x in enumerate(temp_fileobj) if x == indexofptt]
-for i in range(1, len(aavv)):
-    aavv[i] = aavv[i] + 1
+aavv= [i + 2 for i,x in enumerate(temp_fileobj) if x == indexofptt]
 print(aavv)
 print("look at this positions for the variants of patient that you are looking for")
 print("related list <temp_fileobj>")
 for i in aavv:
     print(temp_fileobj[i])
+print(temp_fileobj)
 input("press <enter> to exit")
 
